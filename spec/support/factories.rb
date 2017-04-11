c8a96b020d89f55e15c3c1e4bf283550c4bf5ec5
Mark Van Akkeren <MarkVanAkkeren@gmail.com>
2017-04-10 18:29:50 -0600

    first test 'passing'

--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -1,3 +1,13 @@
 FactoryGirl.define do
 
+  factory :official do
+    name "Tim Kaine"
+    img_path "tim_kaine.png"
+    price 56_000_000
+    # party will refernce party factory
+
+  end
+
+
+
 end
