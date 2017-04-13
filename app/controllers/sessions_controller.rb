4f2f3190d001f00638475581070cea297320f955
Riley Thompson <rileyt_2000@hotmail.com>
2017-04-13 14:25:47 -0600

    add login/logout functionality
    * add functional log out button
    * add sepearate path for login due to user story link in cart index
    * add full user authentication with bcrypt

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,33 @@
+class SessionsController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+    user = User.find_by(username: username)
+
+    if user.authenticate(password)
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      flash.now[:failure] = "Login unsuccessful."
+      render :new
+    end
+  end
+
+  def destroy
+    session.clear
+    redirect_to root_path
+  end
+
+  private
+
+  def username
+    params[:session][:username]
+  end
+
+  def password
+    params[:session][:password]
+  end
+end
