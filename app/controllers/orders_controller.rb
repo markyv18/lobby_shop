3a078261ef7eebf0d0aad66f760ef19c6a1ebd1f
NicholasJacques <nicholas.d.jacques@gmail.com>
2017-04-15 14:03:57 -0600

    completes model test for method add_items_to_order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,21 +10,12 @@ class OrdersController < ApplicationController
 
   def create
     @order = Order.create(total_price: @cart.items_total, user_id: current_user.id)
-    add_items_to_order
+    @order.add_items_to_order(@cart)
     reset_cart
     redirect_to orders_path
   end
 
-#-----Where should we put these methods?
-
-  def add_items_to_order
-    @cart.contents.each do |scumbag_id, quantity|
-      scumbag = Scumbag.find(scumbag_id.to_i)
-      @order.scumbag_orders.create(scumbag_id: scumbag.id,
-                                   scumbag_quantity: quantity,
-                                   scumbag_price: scumbag.price)
-    end
-  end
+  private
 
   def reset_cart
     session[:cart] = {}
