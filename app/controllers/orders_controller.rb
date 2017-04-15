7d66727dac6ace45364c4d1a02d019e2f0625656
NicholasJacques <nicholas.d.jacques@gmail.com>
2017-04-14 23:48:27 -0600

    adds checkout functionality
    saves cart as an order, relationships work
    still need to implement clearing cart
    need to implement order index page
    need to style order view page
    need to check that we have all necessary order methods

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,4 +7,19 @@ class OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
   end
+
+  def create
+    @order = Order.create(total_price: @cart.items_total, user_id: current_user.id)
+    add_items_to_order
+    redirect_to orders_path
+  end
+
+  def add_items_to_order
+    @cart.contents.each do |scumbag_id, quantity|
+      scumbag = Scumbag.find(scumbag_id.to_i)
+      @order.scumbag_orders.create(scumbag_id: scumbag.id,
+                                   scumbag_quantity: quantity,
+                                   scumbag_price: scumbag.price)
+    end
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(username: username)
 
-    if user.authenticate(password)
+    if user && user.authenticate(password)
       session[:user_id] = user.id
       redirect_to root_path
     else
