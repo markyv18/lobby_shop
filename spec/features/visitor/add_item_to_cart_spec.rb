dde555643de89b5e88ff62acae07317912dba94b
NicholasJacques <nicholas.d.jacques@gmail.com>
2017-04-15 18:13:36 -0600

    fixes add_item_to_cart_spec

--- spec/features/visitor/add_item_to_cart_spec.rb
+++ spec/features/visitor/add_item_to_cart_spec.rb
@@ -8,9 +8,8 @@ RSpec.feature "visit '/scumbags' and add item to cart" do
 
     visit scumbags_path
     click_on "Add to Cart"
-
+    save_and_open_page
     within('.flash-notice') { expect(page).to have_content("Added #{scumbag.name} to cart.") }
-    # expect(page).to have_content("Cart: 1")
   end
 
   scenario "can view cart after adding item and see item in cart" do
@@ -24,23 +23,5 @@ RSpec.feature "visit '/scumbags' and add item to cart" do
     expect(page).to have_content(scumbag.name)
     expect(page).to have_content(scumbag.price)
     expect(page).to have_content(scumbag.branch.name)
-    # expect(page).to have_content('Quantity: 1')
-    # test for css element image
-    # total price
   end
 end
-
-
-
-
-  #   title: Adding items to the cart
-  #   body: >
-  #     Background: Items, and a user that is not logged in
-  #     As a visitor
-  #     When I visit any page with an item on it
-  #     I should see a link or button for "Add to Cart"
-  #     When I click "Add to cart" for that item
-  #     And I click a link or button to view cart
-  #     And my current path should be "/cart"
-  #     And I should see a small image, title, description and price for the item I just added
-  #     And there should be a "total" price for the cart that should be the sum of all items in the cart
