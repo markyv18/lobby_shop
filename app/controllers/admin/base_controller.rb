46518979973b02b089f92e79ebf13b2ca7a5746b
NicholasJacques <nicholas.d.jacques@gmail.com>
2017-04-16 17:41:21 -0600

    ordered ordered by created_at: :desc

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -3,9 +3,9 @@ class Admin::BaseController < ApplicationController
 
   def show
     if params[:status]
-      @orders = Order.where(status: params[:status])
+      @orders = Order.where(status: params[:status]).order(created_at: :desc)
     else
-      @orders = Order.all
+      @orders = Order.order(created_at: :desc)
     end
     @orders_count = Order.all.count
     @statuses = Order.statuses
